93cc8415d992adec40a9462a4fbbaa98efffcf22
emrekocadere <emrekocadere34@gmail.com>
2023-10-30 00:01:43 +0300

    Refactor application setup and authentication

--- GoogleMicrosoftAuthenticator/Program.cs
+++ GoogleMicrosoftAuthenticator/Program.cs
@@ -1,19 +1,22 @@
 using GoogleMicrosoftAuthenticator;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<GoogleMicrosoftAuthenticator.DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddIdentity<User,Role>().AddEntityFrameworkStores<DbContext>();
+    
+builder.Services.AddScoped<GoogleMicrosoftAuthenticator.AuthenticatorService>();
+builder.Services.AddIdentity<User,Role>().AddEntityFrameworkStores<GoogleMicrosoftAuthenticator.DbContext>().AddDefaultTokenProviders();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())      
 {
     app.UseSwagger();
     app.UseSwaggerUI();
