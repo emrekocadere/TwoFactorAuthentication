95c2d2fd80066b7f216052389759ac11f6e1690c
emrekocadere <emrekocadere34@gmail.com>
2023-10-29 12:36:31 +0300

    Add DbContext.cs for database context

---
+++ GoogleMicrosoftAuthenticator/Data/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoogleMicrosoftAuthenticator;
+
+public class DbContext:IdentityDbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost; Database=sql; User Id=SA; Password=reallyStrongPwd123;TrustServerCertificate=True");
+    }
+}   
